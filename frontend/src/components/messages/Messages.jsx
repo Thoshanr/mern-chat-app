47e813a9699451b914ff47ab4ea4d187cf8fc7b5
Thoshan <thoshanr333@gmail.com>
2025-04-07 11:54:51 +0530

    setup-4

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Message from './Message.jsx'
 import '../../../src/App.css'
+import useGetMessages from '../../hooks/useGetMessages.js'
+import MessageSkeleton from '../skeletons/MessageSkeleton.jsx'
+import { useEffect } from 'react'
+import useListenMessages from '../../hooks/useListenMessages.js'
 const Messages = () => {
+  const {messages, loading} = useGetMessages();
+  useListenMessages();
+  const lastMessageRef = useRef();
+  
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth"});
+    },20);
+  }, [messages]);
+
   return (
     <div className='px-4 flex-1 overflow-auto scrollbar-track-rounded'>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
+       {!loading && 
+       messages.length > 0 && 
+       messages.map((message) => (
+        <div key={message._id}
+           ref = {lastMessageRef}
+        >
+          <Message  message={message} />
+        </div>
+       ))}
+       
+       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />) }
+
+       {!loading && messages.length === 0 && (
+        <p className='text-center text-gray-400'>Send a message to start the conversation</p>
+       )}
     </div>
   )
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,12 @@ export default defineConfig({
   plugins: [
     tailwindcss()
   ],
+  server: {
+    port:3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+      },
+    }
+  },
 });
